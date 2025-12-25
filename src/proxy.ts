495060c5d3db38d7d7039237e0a0774f9ca58c0f
lobna-eng <lobnamoustafa760@gmail.com>
2025-12-25 16:23:35 +0200

    third  commit

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,8 +1,13 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
-
-const protectedPages = ["/cart", "/wishlist", "/profile", "/allorders","/address"];
+const protectedPages = [
+  "/cart",
+  "/wishlist",
+  "/profile",
+  "/allorders",
+  "/address",
+];
 const authPages = ["/login", "/register"];
 export default async function proxy(req: NextRequest) {
   const token = await getToken({ req });
@@ -19,13 +24,11 @@ export default async function proxy(req: NextRequest) {
   }
 
   if (authPages.includes(req.nextUrl.pathname)) {
-    {
-      if (!token) {
-        return NextResponse.next();
-      } else {
-        const redirectUrl = new URL("/", process.env.NEXTAUTH_URL);
-        return NextResponse.redirect(redirectUrl);
-      }
+    if (!token) {
+      return NextResponse.next();
+    } else {
+      const redirectUrl = new URL("/", process.env.NEXTAUTH_URL);
+      return NextResponse.redirect(redirectUrl);
     }
   }
   return NextResponse.next();
